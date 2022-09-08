eb09224f016b8771622f04bb1908805118019007
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2022-09-08 18:13:11 -0500

    POST and GET's methods to Payment

---
+++ controllers/payment.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"log"
+	"time"
+
+	"github.com/AlexVonEinzbern/go-rest-api/db"
+	"github.com/AlexVonEinzbern/go-rest-api/models"
+)
+
+//TODO: Implement CreatePayment
+func CreatePayment(createpayment models.PaymentCreate) models.Payment {
+
+	payment := models.Payment{
+		ID: createpayment.ID,
+		PaymentBase: models.PaymentBase{
+			OrderID:      createpayment.OrderID,
+			CreditCardID: createpayment.CreditCardID}}
+
+	conn := db.DBConnection()
+	sqlconn, err := conn.DB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sqlconn.Close()
+
+	log.Println("Starting with login creation")
+
+	result := conn.Create(&payment)
+
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+
+	log.Println("This is the record inserted: ", payment)
+
+	return payment
+}
+
+//TODO: Implement SearchPayment
+func SearchPayment(id string) (models.Payment, error) {
+	var payment models.Payment
+	conn := db.DBConnection()
+	sqlconn, err := conn.DB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sqlconn.Close()
+
+	log.Println("Starting with payment searching by id")
+
+	result := conn.First(&payment, "id = ?", id)
+
+	if result.Error != nil {
+		log.Println("DB error", result.Error)
+		return payment, result.Error
+	}
+
+	log.Println("Found record: ", payment)
+	return payment, result.Error
+}
+
+//TODO: Implement SearchPaymentDate
+func SearchPaymentDate(date string) ([]models.Payment, error) {
+	var payments []models.Payment
+	conn := db.DBConnection()
+	sqlconn, err := conn.DB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sqlconn.Close()
+
+	start_date, _ := time.Parse("2006-01-02", date)
+	end_date := start_date.AddDate(0, 0, 1)
+
+	log.Println("Starting with payments searching by date")
+
+	result := conn.Where("created_at between ? and ?", start_date, end_date).Find(&payments)
+
+	if result.Error != nil {
+		log.Println("DB error", result.Error)
+		return payments, result.Error
+	}
+
+	log.Println("Found record: ", payments)
+	return payments, result.Error
+}
---
+++ routes/payment.routes.go
@@ -0,0 +1,87 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/AlexVonEinzbern/go-rest-api/controllers"
+	"github.com/AlexVonEinzbern/go-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+//TODO: Implement CreatePayment
+
+// @BasePath /api/v1
+
+// CreatePayment godoc
+// @Summary Create Payment
+// @Description Create a Payment
+// @Accept  json
+// @Produce  json
+// @Param category body models.PaymentCreate true "Payment type"
+// @Success 200 {object} models.PaymentResponse
+// @Failure 404 {object} models.APIError "Can not find objects"
+// @Router /go-rest-api/payments [post]
+func CreatePayment(c *gin.Context) {
+
+	var createpayment models.PaymentCreate
+
+	err := c.BindJSON(&createpayment)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Starting with Payment creation: ", createpayment)
+	c.IndentedJSON(http.StatusOK, controllers.CreatePayment(createpayment))
+}
+
+//TODO: Implement SearchPayment
+
+// SearchPayment godoc
+// @Summary Search Payment by Customer id
+// @Description Search Payment by id in the DataBase
+// @Accept  json
+// @Produce  json
+// @Param id path string true "Payment ID" default(cabckbalgaLJHALncas)
+// @Success 200 {object} []models.PaymentResponse
+// @Failure 404 {object} models.APIError "Can not find objects"
+// @Router /go-rest-api/payments/{id} [get]
+func SearchPayment(c *gin.Context) {
+
+	id := c.Param("id")
+
+	payment, err := controllers.SearchPayment(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, models.APIError{ErrorCode: 404, ErrorMessage: "Not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, payment)
+}
+
+//TODO: Implement SearchPaymentDate
+
+// SearchPaymentDate godoc
+// @Summary Search Payments by Date
+// @Description Search Payments by date in the DataBase
+// @Accept  json
+// @Produce  json
+// @Param date path string true "Payment date" default(2006-01-02)
+// @Success 200 {object} []models.PaymentResponse
+// @Failure 404 {object} models.APIError "Can not find objects"
+// @Router /go-rest-api/payments/{date} [get]
+func SearchPaymentDate(c *gin.Context) {
+
+	datepayment := c.Param("date")
+
+	payment, err := controllers.SearchLoginDate(datepayment)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, models.APIError{ErrorCode: 404, ErrorMessage: "Not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, payment)
+}
