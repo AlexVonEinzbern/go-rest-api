64b9a2f3c2d769c43da84628af69353f116e60a3
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2022-09-09 16:28:36 -0500

    Some tests for categories methods

---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostCategory(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+
+    input := strings.NewReader(`{
+        "id": "bcajbavajbvajbvbjlaknLHLKUCTG",
+        "category_name": "categoria 1",
+        "description": "description 1"
+    }`)
+
+	req, _ := http.NewRequest("POST", "http://localhost:8080/go-rest-api/categories", input)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestGetCategory(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "http://localhost:8080/go-rest-api/categories", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestGetActiveCategory(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "http://localhost:8080/go-rest-api/categories/active", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
